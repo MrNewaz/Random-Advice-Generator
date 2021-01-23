a3d4e65e48d511868e8393845daf31f15e425f3f
MrNewaz <saifur.saif@northsouth.edu>
2021-01-24 00:17:54 +0600

    init

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-fetch("https://api.adviceslip.com/advice")
-  .then((result) => result.json())
-  .then((data) => console.log(data));
-
 const button = document.querySelector("button");
 const h1 = document.querySelector("h1");
+const pp = document.querySelector(".p");
 
 button.addEventListener("click", () => {
   fetch("https://api.adviceslip.com/advice")
@@ -11,4 +8,8 @@ button.addEventListener("click", () => {
     .then((data) => {
       h1.innerText = data.slip.advice;
     });
+});
+
+pp.addEventListener("click", () => {
+  window.location.reload();
 });
